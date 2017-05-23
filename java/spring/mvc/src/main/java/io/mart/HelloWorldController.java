96edef579c70a3240df2c7862237272b0a8156ca
Alexander Martyushov <alexander.martyushov@bitplaces.com>
2017-05-23 09:52:20 +0200

    Spring mvc simple app

---
+++ java/spring/mvc/src/main/java/io/mart/HelloWorldController.java
@@ -0,0 +1,26 @@
+package io.mart;
+
+import org.springframework.web.servlet.mvc.AbstractController;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * http://localhost:8080/spring2/welcome.htm
+ * @author Aleksandr Martiushov
+ */
+public class HelloWorldController extends AbstractController {
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+                                                 HttpServletResponse response) throws Exception {
+        // To identify which view should return back to the user, in this example HelloWorldPage.jsp will be returned.
+        ModelAndView model = new ModelAndView("HelloWorldPage");
+
+        // Add a “hello world” string into a model named “msg”
+        model.addObject("msg", "hello world");
+
+        return model;
+    }
+}
