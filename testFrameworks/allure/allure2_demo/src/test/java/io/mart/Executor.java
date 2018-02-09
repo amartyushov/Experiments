27a036578e1f1c1f24c0f0e7a95e476780eb5f21
Aleksandr Martiushov <amartyushov@gmail.com>
2018-02-09 14:22:25 +0100

    allure_2: formatting for java is corrected

--- testFrameworks/allure/allure2_demo/src/test/java/io/mart/Executor.java
+++ testFrameworks/allure/allure2_demo/src/test/java/io/mart/Executor.java
@@ -1,33 +1,34 @@
-                package io.mart;
+package io.mart;
 
-        import io.qameta.allure.Attachment;
-        import io.qameta.allure.Step;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
 
-                public class Executor {
+public class Executor {
 
-                    @Step("Login to the system")
-                    public static void login(String name, String password){
-                        enterName(name);
-                        enterPassword(password);
-                    }
+    @Step("Login to the system")
+    public static void login(String name, String password) {
+        enterName(name);
+        enterPassword(password);
+    }
 
-                    @Step("Enter user name: {0}")
-                    @Attachment
-                    private static String enterName(String name){
-                        return name;
-                    }
+    @Step("Enter user name: {0}")
+    @Attachment
+    private static String enterName(String name) {
+        return name;
+    }
 
-                    @Step("Enter user password: {0}")
-                    @Attachment
-                    private static String enterPassword(String password){
-                        return password;
-                    }
+    @Step("Enter user password: {0}")
+    @Attachment
+    private static String enterPassword(String password) {
+        return password;
+    }
 
-                    @Step("Cleanup test data")
-                    public static void cleanupData() { }
+    @Step("Cleanup test data")
+    public static void cleanupData() {
+    }
 
-                    @Step("Create message with title: {0}")
-                    public static String createMessage(String title){
-                        return title;
-                    }
-                }
+    @Step("Create message with title: {0}")
+    public static String createMessage(String title) {
+        return title;
+    }
+}
