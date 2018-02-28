3181d4b59cd88c1fe4ec7d86473d1a89fd614cd2
Aleksandr Martiushov <amartyushov@gmail.com>
2018-02-28 15:13:23 +0100

    testng allure gradle with exceptions

--- testFrameworks/allure/testng_gradle_plugin/src/test/java/io/mart/SimpleTest.java
+++ testFrameworks/allure/testng_gradle_plugin/src/test/java/io/mart/SimpleTest.java
@@ -1,6 +1,7 @@
 package io.mart;
 
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SimpleTest {
@@ -8,5 +9,15 @@ public class SimpleTest {
     @Test
     public void test() {
         System.out.println("This is a test");
+    }
+
+    @Test
+    public void testAssertion() {
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void test2() {
+        throw new RuntimeException("my exception");
     }
 }
