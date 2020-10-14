47ef8a42d468c2dad6d034b93669370b4c57c63b
Aleksandr Martiushov <amartyushov@gmail.com>
2020-10-14 17:47:49 +0200

    Dijkstra impl

---
+++ java/algo/src/main/java/io/mart/data/structures/BSTHeight.java
@@ -0,0 +1,65 @@
+package io.mart.data.structures;
+
+import java.util.Scanner;
+
+public class BSTHeight {
+	
+	static class Node {
+		
+		Node left;
+		Node right;
+		int data;
+		
+		
+		Node(int data) {
+			this.data = data;
+			left = null;
+			right = null;
+		}
+	}
+	
+	
+	/*
+	class Node
+		int data;
+		Node left;
+		Node right;
+	*/
+	public static int height(Node root) {
+		if (root == null) return -1;
+		return Math.max(height(root.left),
+				height(root.right))+1;
+	}
+	
+	
+	public static Node insert(Node root, int data) {
+		if (root == null) {
+			return new Node(data);
+		} else {
+			Node cur;
+			if (data <= root.data) {
+				cur = insert(root.left, data);
+				root.left = cur;
+			} else {
+				cur = insert(root.right, data);
+				root.right = cur;
+			}
+			return root;
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int t = scan.nextInt();
+		Node root = null;
+		while (t-- > 0) {
+			int data = scan.nextInt();
+			root = insert(root, data);
+		}
+		scan.close();
+		int height = height(root);
+		System.out.println(height);
+	}
+}
+
